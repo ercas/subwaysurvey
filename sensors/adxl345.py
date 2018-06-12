71cd6411e1985623b7decf58efa11d42efd3f1ba
ercas <edgarrimandocastro@gmail.com>
2018-06-11 23:01:16 -0400

    offload timestamping to the sensor to avoid overhead

--- sensors/adxl345.py
+++ sensors/adxl345.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python3
 
+from matplotlib import pyplot
+import colorama
 import serial
 import sys
 import time
 
 OUTPUT = "adxl345_%d.csv" % time.time()
+THRESH = 20
+GRAPH_WIDTH = 70
 
 device_file = sys.argv[1]
 adxl345 = serial.Serial(
     device_file,
-    baudrate = 9600,
+    baudrate = 14400,
     parity = serial.PARITY_NONE,
     stopbits = serial.STOPBITS_ONE,
     xonxoff = False
 )
 
-print("writing data to %s" % OUTPUT)
 with open(OUTPUT, "w") as f:
     f.write("TIME,X,Y,Z\n")
     while True:
         try:
-            line = "%f,%s" % (
-                time.time(), adxl345.readline().rstrip().decode()
-            )
+            line = adxl345.readline().rstrip().decode()
             if ("X" in line):
                 pass
             else:
+                (timestamp, x, y, z) = line.split(",")
                 f.write("%s\n" % line)
-                print(line)
+
+                (x_float, y_float, z_float) = map(
+                    lambda n: abs(float(n)),
+                    (x, y, z)
+                )
+                (x_width, y_width, z_width) = map(
+                    lambda n: int(n / THRESH * GRAPH_WIDTH) - 1,
+                    (x_float, y_float, z_float)
+                )
+                print(
+                    colorama.Fore.RED
+                    + ("% 6.2f | " % x_float)
+                    + ("█" * x_width)
+                )
+                print(
+                    colorama.Fore.GREEN
+                    + ("% 6.2f | " % y_float)
+                    + ("█" * y_width)
+                )
+                print(
+                    colorama.Fore.BLUE
+                    + ("% 6.2f | " % z_float)
+                    + ("█" * z_width)
+                )
+                #print(line)
         except KeyboardInterrupt:
             break
         except UnicodeDecodeError:
+            pass
+        except ValueError:
             pass
